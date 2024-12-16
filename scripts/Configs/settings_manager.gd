72fcd5e891fd5b15591201b0dbe19fb1b9ac84ea
sharpoff <sharpyeah@gmail.com>
2024-12-16 17:29:25 +0900

    fix character animation, started to work on state machine

--- scripts/Configs/settings_manager.gd
+++ scripts/Configs/settings_manager.gd
@@ -3,6 +3,5 @@ extends Node
 var master_volume: float = 100.0
 var sound_effects_volume: float = 100.0
 
-enum {RUSSIAN, ENGLISH}
-
-var language = ENGLISH
+enum Languages {ENGLISH, RUSSIAN}
+var current_state = Languages.ENGLISH
--- scripts/UI/options_menu.gd
+++ scripts/UI/options_menu.gd
@@ -2,10 +2,17 @@ extends Control
 
 @export var click: AudioStreamPlayer2D
 
+
 func _on_back_button_button_up() -> void:
 	click.play()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
-func _on_h_slider_value_changed(value: float) -> void:
-	pass
+func _on_language_options_item_selected(index: int) -> void:
+	SettingsManager.current_state = index
+
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	SettingsManager.master_volume = value
+
+func _on_sound_effects_slider_value_changed(value: float) -> void:
+	SettingsManager.sound_effects = value
