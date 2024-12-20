81cbe237c1b44579c0cdc4226c69f1de3ceaef9f
sharpoff <sharpyeah@gmail.com>
2024-12-20 18:30:09 +0900

    add scene manager script, add fishing animation

--- scripts/game/door.gd
+++ scripts/game/door.gd
@@ -5,4 +5,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print_debug("Entered")
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file(scene_to_transit)
+		pass
+		#get_tree().change_scene_to_file(scene_to_transit) # redundant
--- scripts/game/map.gd
+++ scripts/game/map.gd
@@ -2,9 +2,5 @@ extends Node2D
 
 @export var player_scene: PackedScene
 
-
 func _ready() -> void:
 	add_child(player_scene.instantiate())
-
-func _process(_delta: float) -> void:
-	pass
