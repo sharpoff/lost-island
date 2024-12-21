e84422526c9065ee0211091255a39696a27d3a31
void <103306045+sharpoff@users.noreply.github.com>
2024-12-21 16:01:25 +0900

    add error message in lobby selection

--- scripts/ui/options_menu.gd
+++ scripts/ui/options_menu.gd
@@ -1,13 +1,18 @@
 extends Control
 
-enum back_button_func_type {MAIN_MENU, HIDE}
-@export var back_button_func: back_button_func_type
 var main_menu_scene = "res://scenes/ui/menu_main.tscn"
+@export var master_vol_slider: HSlider
+@export var sfx_vol_slider: HSlider
+@export var language_option: OptionButton
 
+
+func _ready() -> void:
+	language_option.selected = SettingsManager.language
+	master_vol_slider.value = SettingsManager.master_volume
+	sfx_vol_slider.value = SettingsManager.sound_effects_volume
 
 func _on_language_options_item_selected(index: int) -> void:
-	@warning_ignore("int_as_enum_without_cast")
-	SettingsManager.current_state = index
+	SettingsManager.language = index
 
 func _on_master_volume_slider_value_changed(value: float) -> void:
 	SettingsManager.master_volume = value
